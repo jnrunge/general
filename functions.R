d5ee668f2d0f172f288eaf116aca3ea89b7a5129
Jan-Niklas Runge <33123209+jnrunge@users.noreply.github.com>
2023-03-22 09:52:43 +0100

    Update functions.R
    added slurm_check_jobs_still_running

--- functions.R
+++ functions.R
@@ -179,3 +179,7 @@ start_sbatch_list=function(sbatch_list, jobs_simul, jobname, initial_timedate){
     print(system(command=paste("Rscript ~/BrusselSprouts/scripts/Execute_Sbatches.R '",initial_timedate,"' ",sbatch_list," ",jobs_simul," ",jobname, sep=""), intern=TRUE))
     }
 }
+
+slurm_check_jobs_still_running=function(username,jobname){
+    return(length(system(command=paste0("sacct -u ",username," --name=",jobname," | grep -v COM | grep -v FAI | cut -f 1 -d' ' | grep -v ba | grep '^[0-9]'"),intern=TRUE)) > 0))
+}
